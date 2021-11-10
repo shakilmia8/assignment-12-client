7c031c20b344d104a080c229f6cb6de3d6947f5b
shakilmia8 <shakilmia8@gmail.com>
2021-11-10 16:12:03 +0600

    products commit

--- src/pages/Home/Cars/Cars.js
+++ src/pages/Home/Cars/Cars.js
@@ -1,5 +1,57 @@
 import React from 'react';
 
+const products = [
+    {
+        key: 'car001',
+        name: 'Volvo XC60',
+        price: '61.9 Lakh',
+        launched: '19-Oct-2021',
+        describe: "The Volvo XC60 is a compact luxury crossover SUV manufactured and marketed by Swedish automaker Volvo Cars since 2008. ... The XC60 is part of Volvo's 60 Series of automobiles, along with the S60, S60 Cross Country, V60, and V60 Cross Country. The first generation model introduced a new style for the 60 Series models.",
+        img: 'https://static.autox.com/uploads/cars/2021/10/volvo-xc60.jpg'
+    },
+    {
+        key: 'car002',
+        name: 'Volvo S90',
+        price: '61.9 Lakh',
+        launched: '19-Oct-2021',
+        describe: "With a distinctive design inside and out and a plush driving experience, the Volvo S90 is a luxury sedan that prides itself on being different from the German competition. Its minimalist Scandinavian style makes for a cosseting cabin, and the quiet and comfortable ride foregoes sportiness in favor of relaxation.",
+        img: 'https://static.autox.com/uploads/cars/2021/10/volvo-s901.jpg'
+    },
+    {
+        key: 'car003',
+        name: 'Tata Punch',
+        price: '5.49 Lakh',
+        launched: '18-Oct-2021',
+        describe: "Tata Punch is a 5 seater Compact SUV available in a price range of ₹ 5.49 - 9.39 Lakh. It is available in 14 variants, 1 engine option and 2 transmission options : Manual and AMT. Other key specifications of the Punch include a Ground Clearance of 187 mm and Bootspace of 366 litres. The Punch is available in 7 colours.",
+        img: 'https://static.autox.com/uploads/cars/2021/10/tata-punch.jpg'
+    },
+    {
+        key: 'car004',
+        name: 'MG Astor',
+        price: '9.78 Lakh',
+        launched: '11-Oct-2021',
+        describe: "MG Astor is a 5 seater SUV available in a price range of ₹ 9.78 - 17.38 Lakh. It is available in 12 variants, 1 engine option and 3 transmission options : Manual, Automatic (CVT) and Automatic (Torque Converter). Other key specifications of the Astor include a Kerb Weight of 1245 kg.",
+        img: 'https://static.autox.com/uploads/cars/2021/10/mg-astor.jpghttps://static.autox.com/uploads/cars/2021/10/mg-astor.jpg'
+    },
+    {
+        key: 'car005',
+        name: 'Mahindra XUV700',
+        price: '11.99 Lakh',
+        launched: '30-Sep-2021',
+        describe: "Mahindra XUV700 is a 7 seater SUV available in a price range of Rs. 12.49 - 22.99 Lakh*. It is available in 23 variants, 2 engine options that are BS6 compliant and 2 transmission options: Manual & Automatic. Other key specifications of the XUV700 include a kerb weight of and boot space of Liters.",
+        img: 'https://static.autox.com/uploads/cars/2021/10/mahindra-xuv700.jpg'
+    },
+    {
+        key: 'car006',
+        name: 'Volkswagen Taigun',
+        price: '10.49 Lakh',
+        launched: '23-Sep-2021',
+        describe: "Volkswagen Taigun is a 5 seater SUV available in a price range of ₹ 10.49 - 17.50 Lakh. In the BS6 era, the Taigun is powered by 999 cc Petrol engine which produces 114bhp of power and 178Nm of torque. The 1498 cc Petrol engine which produces 148bhp of power and 250Nm of torque.",
+        img: 'https://static.autox.com/uploads/cars/2021/09/volkswagen-taigun3.jpg'
+    },
+]
+
+
 const Cars = () => {
     return (
         <div>
